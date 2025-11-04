055e96bde7953a92fb86d958555da4f453166354
Devang Patel <51948885+Devang96870@users.noreply.github.com>
2025-11-04 14:16:36 +0530

    Updated build process

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-
-  // 👇 This is critical for GitHub Pages
-  base: '/searchspring-frontend/',
-
-  build: {
-    outDir: 'dist',
-    sourcemap: false, // Optional: set to true if you want debug maps
-  },
-
-  server: {
-    port: 5173, // default dev server port
-    open: true, // auto open browser on npm run dev
-  },
 })
